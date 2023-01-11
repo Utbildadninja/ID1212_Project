84b24fee69fb4f2e57ae05cd7175842d764636fe
Vildsvinet <tove.verner.carlsson@gmail.com>
2023-01-11 15:07:14 +0100

    updates in OWDAO
    settings funktions

--- src/main/java/kth/se/id1212/integration/OtherWordsDAO.java
+++ src/main/java/kth/se/id1212/integration/OtherWordsDAO.java
@@ -33,11 +33,11 @@ public class OtherWordsDAO {
     /**
      * Constructor
      */
-    public OtherWordsDAO(){
-        try{
+    public OtherWordsDAO() {
+        try {
             connectToDB();
             prepareStatements();
-        } catch (ClassNotFoundException | SQLException e){
+        } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
             System.out.println("Couldn't connect to db, " + e.getMessage());
         }
@@ -45,20 +45,20 @@ public class OtherWordsDAO {
 
     /**
      * Find the available languages to play the game in
+     *
      * @return list of language beans, one per available language (ID, languagename)
      */
-    public ArrayList<LanguageBean> findLanguages(){
+    public ArrayList<LanguageBean> findLanguages() {
         ArrayList<LanguageBean> languages = new ArrayList<>();
         ResultSet resultSet = null;
         try {
             resultSet = findLanguagesStmt.executeQuery();
-            while (resultSet.next()){
+            while (resultSet.next()) {
                 languages.add(new LanguageBean(resultSet.getInt(1), resultSet.getString(2)));
             }
             connection.commit();
         } catch (SQLException e) {
             exceptionHandler("Couldn't execute query: ", e);
-            throw new RuntimeException(e);
         } finally {
             closeResult(resultSet);
         }
@@ -67,20 +67,21 @@ public class OtherWordsDAO {
 
     /**
      * Find the user reports about inappropriate words
+     *
      * @return list of ReportBeans, one per user report
      */
-    public ArrayList<ReportBean> findReports(){
+    public ArrayList<ReportBean> findReports() {
         ArrayList<ReportBean> reports = new ArrayList<>();
         ResultSet resultSet = null;
         try {
             resultSet = findReportsStmt.executeQuery();
-            while (resultSet.next()){
+            while (resultSet.next()) {
                 reports.add(new ReportBean());  //TODO add properties to the ReportBeans
             }
             connection.commit();
         } catch (SQLException e) {
             exceptionHandler("Couldn't execute reports query: ", e);
-            throw new RuntimeException(e);
+            e.printStackTrace();
         } finally {
             closeResult(resultSet);
         }
@@ -93,13 +94,13 @@ public class OtherWordsDAO {
      * @param userID the ID of the logged-in user
      * @return list of
      */
-    public SettingsBean findUserSettings(int userID){
+    public SettingsBean findUserSettings(int userID) {
         SettingsBean userSettings = null;
         ResultSet resultSet = null;
         try {
             findSettingsStmt.setInt(1, userID);
             resultSet = findSettingsStmt.executeQuery();
-            if(resultSet.next()){
+            if (resultSet.next()) {
                 userSettings = new SettingsBean(
                         resultSet.getInt("language_id"), resultSet.getInt("secondsPerRound"),
                         resultSet.getInt("roundsPerGame")
@@ -108,7 +109,6 @@ public class OtherWordsDAO {
             connection.commit();
         } catch (SQLException e) {
             exceptionHandler("Couldn't execute settings query: ", e);
-            throw new RuntimeException(e);
         } finally {
             closeResult(resultSet);
         }
@@ -117,14 +117,15 @@ public class OtherWordsDAO {
 
     /**
      * Find all users, ordered by number of games played (to display "most frequent players" highscore)
+     *
      * @return list of UserBeans
      */
-    public ArrayList<UserBean> findAllUsersByPlayedGames(){
+    public ArrayList<UserBean> findAllUsersByPlayedGames() {
         ArrayList<UserBean> users = new ArrayList<>();
         ResultSet resultSet = null;
         try {
             resultSet = findUsersStmt.executeQuery();
-            while (resultSet.next()){
+            while (resultSet.next()) {
                 users.add(new UserBean(
                         resultSet.getInt("id"), resultSet.getString("username"),
                         resultSet.getBoolean("admin"), resultSet.getInt("gamesPlayed")
@@ -142,11 +143,12 @@ public class OtherWordsDAO {
 
     /**
      * Called with login request, tries to match entered credentials with user in the db
+     *
      * @param username attempted login username
      * @param password attempted login password
      * @return UserBean if user is found in db, null if credentials not matching
      */
-    public UserBean findUser(String username, String password){
+    public UserBean findUser(String username, String password) {
         ResultSet resultSet = null;
         UserBean user = null;
         int resultSize = 0;
@@ -158,8 +160,7 @@ public class OtherWordsDAO {
 
             resultSet = findUserStmt.executeQuery();
 
-            //TODO test if this version works
-            if(resultSet.next()){
+            if (resultSet.next()) {
                 user = new UserBean(resultSet.getInt("id"), resultSet.getString("username"),
                         resultSet.getBoolean("admin"), resultSet.getInt("gamesPlayed")
                 );
@@ -179,15 +180,16 @@ public class OtherWordsDAO {
      * Will return noOfWords words from the database based on randomised wordIDs..
      * TODO a bit fishy not sure it will work, problematic if we have removed words etc,
      * ie some IDs are missing from the DB...
+     *
      * @param noOfWords
      * @param languageID
      * @return
      */
-    public ArrayList<WordBean> words(int noOfWords, int languageID){
+    public ArrayList<WordBean> words(int noOfWords, int languageID) {
         ArrayList<WordBean> words = new ArrayList<>();
         ResultSet resultSetAmount = null;
         ResultSet resultSetWords = null;
-        try{
+        try {
             resultSetAmount = findNoOfWordsStmt.executeQuery();
             //int[] of size noOfWords, with random values(1, resultSetAmount) //very problematic if there are removed words
             //convert to String
@@ -203,13 +205,28 @@ public class OtherWordsDAO {
 
     /**
      * If a logged-in user updates their settings, this should be updated in the db as well
+     *
      * @param userID The user for whom to change personal sessions
      */
-    public void updateSettings(int userID, SettingsBean newSettings){
-
-
+    public void updateSettings(int userID, SettingsBean newSettings) {
+        System.out.println("user wants to update the settings");
+        int affectedRows = 0;
+        try {
+            updateSettingsStmt.setInt(1, newSettings.getLanguageID());
+            updateSettingsStmt.setInt(2, newSettings.getSecondsPerRound());
+            updateSettingsStmt.setInt(3, newSettings.getRoundsPerGame());
+            updateSettingsStmt.setInt(4, userID);
+            affectedRows = updateSettingsStmt.executeUpdate();
+            if (affectedRows != 1) {
+                exceptionHandler("Couldn't update user settings ", null);
+                connection.rollback();
+            }
+            else
+                connection.commit();
+        } catch (SQLException e) {
+            exceptionHandler("Couldn't execute update user settings query: ", e);
+        }
     }
-
 
     private void connectToDB() throws ClassNotFoundException, SQLException {
         Class.forName("org.apache.derby.jdbc.ClientDriver");
@@ -220,7 +237,8 @@ public class OtherWordsDAO {
 
     private void prepareStatements() throws SQLException {
         findLanguagesStmt = connection.prepareStatement(
-                "SELECT * FROM languages"
+                "SELECT * FROM languages " +
+                        "WHERE NOT (languagename='testing' OR languagename='english_api'"
         );
         findReportsStmt = connection.prepareStatement(
                 "SELECT * FROM reports"
@@ -249,25 +267,25 @@ public class OtherWordsDAO {
                 "UPDATE words SET skipped = skipped +1 " +
                         "WHERE id = ?"
         );
-        createReportStmt = connection.prepareStatement(
-                "INSERT INTO reports (timeOfReport, reportingUser, reportedWord, reason)" +
-                        "VALUES (now(), ?, ?, ?)"
-        );
         updateSettingsStmt = connection.prepareStatement(
                 "UPDATE user_settings " +
-                        "SET language_id = ?, secondsPerRound = ?, roundsPerGame = ?" +
+                        "SET language_id = ?, secondsPerRound = ?, roundsPerGame = ? " +
                         "WHERE user_id = ?"
         );
         updateUserGamesStmt = connection.prepareStatement(
-                "UPDATE users SET gamesPlayed = gamesPlayed + 1" +
+                "UPDATE users SET gamesPlayed = gamesPlayed + 1 " +
                         "WHERE id = ?"
         );
+        createReportStmt = connection.prepareStatement(
+                "INSERT INTO reports (timeOfReport, reportingUser, reportedWord, reason) " +
+                        "VALUES (now(), ?, ?, ?)"
+        );
         createUserStmt = connection.prepareStatement(
-                "INSERT INTO users (username, password, admin, gamesplayed)" +
+                "INSERT INTO users (username, password, admin, gamesplayed) " +
                         "VALUES (?, ?, false, 0)"
         );
         createWordStmt = connection.prepareStatement(
-                "INSERT INTO words (word, clue, correctlyGuessed, skipped, flags)" +
+                "INSERT INTO words (word, clue, correctlyGuessed, skipped, flags) " +
                         "VALUES (?, ?, 0, 0, 0)"
         );
         deleteUserStmt = connection.prepareStatement(
@@ -278,7 +296,7 @@ public class OtherWordsDAO {
         );
     }
 
-    private void closeResult(ResultSet result){
+    private void closeResult(ResultSet result) {
         try {
             result.close();
         } catch (SQLException e) {
@@ -287,12 +305,14 @@ public class OtherWordsDAO {
         }
     }
 
-    private void exceptionHandler(String failMsg, Exception exception){
+    private void exceptionHandler(String failMsg, Exception exception) {
         System.out.println(failMsg + exception.getMessage());
-        try{
+        try {
             connection.rollback();
+            exception.printStackTrace();
         } catch (SQLException e) {
             System.out.println("Could not rollback: " + e.getMessage());
+            e.printStackTrace();
             throw new RuntimeException(e);
         }
     }
--- src/main/java/kth/se/id1212/model/SettingsBean.java
+++ src/main/java/kth/se/id1212/model/SettingsBean.java
@@ -38,4 +38,20 @@ public class SettingsBean {
     public void setWordSource(String wordSource) {
         this.wordSource = wordSource;
     }
+
+    public int getLanguageID() {
+        return languageID;
+    }
+
+    public void setLanguageID(int languageID) {
+        this.languageID = languageID;
+    }
+
+    public String getLanguageName() {
+        return languageName;
+    }
+
+    public void setLanguageName(String languageName) {
+        this.languageName = languageName;
+    }
 }
